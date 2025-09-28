9002e25464005bc0754005774d4192e3d5459fb2
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-28 19:14:51 +0530

    Adding in main

---
+++ examples/Water_Ethanol_Demo.py
@@ -0,0 +1,15 @@
+from vle_plotter import plot_vle, plot_multiple_vle
+
+def main():
+    print("Ethanol-Water VLE Visualization")
+
+    # Example A : Single curve at 78 °C
+    print("Plotting at 78 °C.")
+    plot_vle(T=78)
+
+    # Example B : Comparing at multiple temperatures
+    print("Plotting at 60 °C, 78 °C, 90 °C.")
+    plot_multiple_vle([60, 78, 90])
+
+if __name__ == "__main__":
+    main()
---
+++ src/VLE_plotter.py
@@ -0,0 +1,49 @@
+import numpy as np
+# using numpy library
+import matplotlib.pyplot as plt
+# using matplot to plot
+
+# Taking binary system of Water and Ethanol
+# Antoine equation to find P_sat
+# Antoine eq: log10(P_sat) = A - B / (C + T)
+# P in mmHg, T in deg C
+
+antoine_constants = {
+    "water": {"A": 8.07131, "B": 1730.63, "C": 233.426},
+    "ethanol": {"A": 8.20417, "B": 1642.89, "C": 230.3}
+}
+
+def antoine_pressure(A, B, C, T):
+    # For finding P_sat
+    return 10 ** (A - (B / (C + T)))
+
+def vle_curve(T=78):
+
+    # Calculating VLE curve for water-ethanol at constant temperature
+    # Pure component vapor pressures
+
+    P_water   = antoine_pressure(**antoine_constants["water"], T=T)
+    P_ethanol = antoine_pressure(**antoine_constants["ethanol"], T=T)
+
+    x1 = np.linspace(0, 1, 100)   # mole fraction ethanol in liquid
+    y1 = (x1 * P_ethanol) / (x1 * P_ethanol + (1 - x1) * P_water)
+
+    return x1, y1
+
+def plot_vle(T=78):
+    # Plotting x-y VLE curve
+    x1, y1 = vle_curve(T)
+
+    plt.figure(figsize=(6,6))
+    plt.plot(x1, y1, label=f"T = {T} °C (Raoult's Law)")
+    plt.plot([0,1],[0,1], 'k--', label="y = x (ideal line)")
+
+    plt.xlabel("Liquid Mole Fraction Ethanol (x1)")
+    plt.ylabel("Vapor Mole Fraction Ethanol (y1)")
+    plt.title("VLE Diagram: Ethanol-Water (Raoult's Law)")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    plot_vle()
