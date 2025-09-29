1fd1250bf13eb6248e1fb2de32874eaaf862ea1b
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-29 19:16:03 +0530

    Update Water_Ethanol_Demo.py

--- examples/Water_Ethanol_Demo.py
+++ examples/Water_Ethanol_Demo.py
@@ -4,7 +4,7 @@ import sys, os
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
 
-from src.vle_plotter import plot_vle, plot_multiple_vle
+from src.vle_plotter import plot_vle #, plot_multiple_vle
 
 def main():
     print("Ethanol-Water VLE Visualization")
@@ -14,8 +14,8 @@ def main():
     plot_vle(T=78)
 
     # Example B : Comparing at multiple temperatures
-    print("Plotting at 60 °C, 78 °C, 90 °C.")
-    plot_multiple_vle([60, 78, 90])
+    # print("Plotting at 60 °C, 78 °C, 90 °C.")
+    # plot_multiple_vle([60, 78, 90])
 
 if __name__ == "__main__":
     main()
