7c03e730a2937ed73a95c236c3a4f50998a6f7a5
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-29 19:11:18 +0530

    Update Water_Ethanol_Demo.py

--- examples/Water_Ethanol_Demo.py
+++ examples/Water_Ethanol_Demo.py
@@ -1,3 +1,9 @@
+import sys, os
+
+# Add project root to Python path so it can find "src"
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 from vle_plotter import plot_vle, plot_multiple_vle
 
 def main():
