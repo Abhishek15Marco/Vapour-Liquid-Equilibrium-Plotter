794d43efca2e15ffce63f501446d045aeaf13fde
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-30 20:06:32 +0530

    Update vle_plotter.py

--- src/vle_plotter.py
+++ src/vle_plotter.py
@@ -29,20 +29,18 @@ def vle_curve(T=78):
     y1 = (x1 * P_ethanol) / (x1 * P_ethanol + (1 - x1) * P_water)
     x2 = 1 - x1
     y2 = 1 - y1
-    z1 = P_water
-    z2 = P_ethanol
-    
-    return x1, y1, x2, y2, z1, z2
+  
+    return x1, y1, x2, y2
 
 def plot_vle(T=78):
     # Plotting x-y VLE curve
-    x1, y1, x2, y2, z1, z2= vle_curve(T)
+    x1, y1, x2, y2= vle_curve(T)
 
     plt.figure(figsize=(8,6))
     plt.plot(x1, y1, label=f"T = {T} °C (Vapor Pressure Ethanol)")
-    plt.plot(x1, (x1*z1), 'k--', label="y = x (Liquid Pressure Ethanol)")
+    plt.plot([0,1], [0,1], 'k--', label="y = x (Ideal Line Ethanol)")
     plt.plot(x1, y2, label=f"T = {T} °C (Vapor Pressure Water)")
-    plt.plot(x1,(x1*z2), 'k--', label="y = -x (Liquid Pressure Ethanol)")
+    plt.plot([0,1],[1,0], 'k--', label="y = -x (Ideal Line Water)")
     
     plt.xlabel("Liquid Mole Fraction Ethanol (x1)")
     plt.ylabel("Vapor Mole Fraction Ethanol (y1)")
