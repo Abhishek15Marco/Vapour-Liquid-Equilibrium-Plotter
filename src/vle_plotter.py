5cd434507e09f830c4b6155ba0f8857653cec27c
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-30 19:42:37 +0530

    Update vle_plotter.py

--- src/vle_plotter.py
+++ src/vle_plotter.py
@@ -24,24 +24,22 @@ def vle_curve(T=78):
 
     P_water   = antoine_pressure(**antoine_constants["water"], T=T)
     P_ethanol = antoine_pressure(**antoine_constants["ethanol"], T=T)
-    P_total = P_water + P_ethanol
-    
+        
     x1 = np.linspace(0, 1, 100)   # mole fraction ethanol in liquid
     y1 = (x1 * P_ethanol) / (x1 * P_ethanol + (1 - x1) * P_water)
     x2 = 1 - x1
     y2 = 1 - y1
-    return x1, y1, x2, y2, P_total
+    return x1, y1, x2, y2
 
 def plot_vle(T=78):
     # Plotting x-y VLE curve
-    x1, y1, x2, y2, P_total= vle_curve(T)
+    x1, y1, x2, y2= vle_curve(T)
 
     plt.figure(figsize=(8,6))
     plt.plot(x1, y1, label=f"T = {T} °C (Raoult's Law)")
     plt.plot([0,1],[0,1], 'k--', label="y = x (ideal line)")
     plt.plot(x1, y2, label=f"T = {T} °C (Raoult's Law Water)")
-    plt.plot(x1, P_total, label=f"T = {T} °C (Total Pressure)")
-
+    
     plt.xlabel("Liquid Mole Fraction Ethanol (x1)")
     plt.ylabel("Vapor Mole Fraction Ethanol (y1)")
     plt.title("VLE Diagram: Ethanol-Water (Raoult's Law)")
